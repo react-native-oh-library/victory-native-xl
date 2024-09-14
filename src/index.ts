cc35ae99f6582886b5a32929eddc5232d24ea5ac
hongtenglong <hongtenglong1@h-partners.com>
2024-09-14 15:32:27 +0800

    Fix CartesianChart animation issues

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+/**
+ * Cartesian chart exports (including useful types)
+ */
+export { CartesianChart } from "./cartesian/CartesianChart";
+
+
+export { type CurveType } from "victory-native";
+export { type RoundedCorners } from "victory-native";
+
+export { useAnimatedPath } from "victory-native";
+export { AnimatedPath } from "victory-native";
+export { usePrevious } from "victory-native";
+export {
+  useChartPressState,
+  type ChartPressState,
+} from "victory-native";
+
+// Line
+export { useLinePath } from "victory-native";
+export { Line } from "victory-native";
+
+// Bar
+export { useBarPath } from "victory-native";
+export { Bar } from "victory-native";
+
+// Bar group
+export { useBarGroupPaths } from "victory-native";
+export { BarGroup } from "victory-native";
+
+// Area
+export { useAreaPath } from "victory-native";
+export { Area } from "victory-native";
+
+// Scatter
+export { Scatter, type ScatterShape } from "victory-native";
+
+// Grid and Axis
+export { CartesianAxis } from "victory-native";
+
+/**
+ * Polar chart exports
+ */
+export { PolarChart } from "victory-native";
+
+/**
+ * Pie chart exports (including useful types)
+ */
+export { Pie, type PieSliceData } from "victory-native";
+
+export { useSlicePath } from "victory-native";
+export { useSliceAngularInsetPath } from "victory-native";
